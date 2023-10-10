a82e4f2fd2dc300727f2524254ee679b9a56e62c
mihad-khadem <mihadkhadem@gmail.com>
2023-10-10 21:39:40 +0600

    Project Ready

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_wJIVWL7b1xk1UmcZ-KFKUnH69DaoVPU",
+  authDomain: "fir-introduction-f4dc5.firebaseapp.com",
+  projectId: "fir-introduction-f4dc5",
+  storageBucket: "fir-introduction-f4dc5.appspot.com",
+  messagingSenderId: "1063030628015",
+  appId: "1:1063030628015:web:7aac6edb43135d363ebe5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Pages/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            this is header
+        </div>
+    );
+};
+
+export default Header;
