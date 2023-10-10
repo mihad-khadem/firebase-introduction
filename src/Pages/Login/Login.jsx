9ba33f8f5922f12072b1fea580fa56df43477e26
mihad-khadem <mihadkhadem@gmail.com>
2023-10-11 02:00:04 +0600

    Github authentication

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,26 +1,61 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
+    const gitProvider = new GithubAuthProvider()
+    const [user, setUser] = useState(null)
 
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
         .then(result => {
-            const user = result.user
-            console.log(user);
+            const logedInUser = result.user
+            console.log(logedInUser);
+            setUser(logedInUser)
         })
         .catch(err => {
             console.log('Error' , err.code, err.message);
         })
     }
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, gitProvider)
+        .then(result => {
+            const logedInUser = result.user
+            console.log(logedInUser);
+            setUser(logedInUser);
+        })
+        .catch(err => console.log(err))
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(result => {
+            setUser(null)
+            console.log(result);
+        })
+        .catch(err => console.log(err))
+    }
     return (
         <div>
             <div>
-                <button onClick={() => handleGoogleSignIn()} className="login-btn">Google Login</button>
+                {
+                    user ? <button onClick={handleSignOut} className="login-btn">Sign out</button> : 
+                    <div>
+                        <button onClick={() => handleGoogleSignIn()} className="login-btn">Google Login</button>
+                        <button onClick={() => handleGithubSignIn()}  className="login-btn">GitHub Login</button>
+                    </div> 
+                    
+                }
             </div>
+            {
+                user && <div>
+                    <h2>User Name : {user.displayName}</h2>
+                    <p>user email {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                     </div>
+            }
         </div>
     );
 };
